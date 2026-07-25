595633e28ed39f4c71ce4403a74ca2b1a2730413
Hooman MOD <hnmodeq@gmail.com>
2026-07-25 21:26:41 +0330

    Revert "feat(homepage): Content Hub with Module Strip layout"
    This reverts commit b4417964f034303390ab9f61ed7e986a3b70da7f.

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,5 @@
-import {
-  HeroSection,
-  FeaturedProduct,
-  LatestArticles,
-  LatestVideos,
-  ModuleStrip,
-  HotForumTopics,
-  QuickTools,
-  FinalCTA,
-} from "@/features/home/components/sections";
-import SectionDivider from "@/features/home/components/SectionDivider";
+import LinearInspiredPreview from "@/features/home/components/LinearInspiredPreview";
 
 export default function HomePage() {
-  return (
-    <main className="min-h-screen">
-      {/* 1. Hero - Big statement */}
-      <HeroSection />
-      
-      <SectionDivider />
-
-      {/* 2. Featured Product - Shop priority */}
-      <FeaturedProduct />
-      
-      <SectionDivider />
-
-      {/* 3. Latest Articles - Magazine */}
-      <LatestArticles />
-      
-      <SectionDivider />
-
-      {/* 4. Latest Videos - Reels */}
-      <LatestVideos />
-      
-      <SectionDivider />
-
-      {/* 5. Module Strip - All modules at a glance */}
-      <ModuleStrip />
-      
-      <SectionDivider />
-
-      {/* 6. Hot Forum Topics */}
-      <HotForumTopics />
-      
-      <SectionDivider />
-
-      {/* 7. Quick Tools */}
-      <QuickTools />
-      
-      <SectionDivider />
-
-      {/* 8. Final CTA */}
-      <FinalCTA />
-    </main>
-  );
+  return <LinearInspiredPreview />;
 }
