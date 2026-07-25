c8b97f3810ba110dac44a1affee03013c6acd823
Hooman MOD <hnmodeq@gmail.com>
2026-07-25 21:26:41 +0330

    Revert "feat(homepage): Content Hub with Module Strip layout"
    This reverts commit 32857ddb1edd0bb026bb178d4f17675bc9d0cbad.

--- features/home/components/SectionDivider.tsx
+++
@@ -1,9 +0,0 @@
-export default function SectionDivider() {
-  return (
-    <div className="h-px w-full" 
-      style={{
-        background: "linear-gradient(90deg, transparent 0%, oklch(1 0 0 / 8%) 50%, transparent 100%)"
-      }} 
-    />
-  );
-}
